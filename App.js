b8076882da14602d6d2eb8bad734aa245dc2f0c7
NamalTharindu97 <NamalA701997@gmail.com>
2023-03-04 09:44:38 +0530

    write file function

--- App.js
+++ App.js
@@ -5,3 +5,8 @@ fs.readFile("file.txt", "utf8", function (err, data) {
   if (err) throw err;
   console.log(data);
 });
+
+fs.writeFile("file.txt", "hello world namal ", function (err) {
+  if (err) throw err;
+  console.log("file saved");
+});
